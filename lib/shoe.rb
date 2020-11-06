80ccee1c7de7f9c2f293e78c56f1090c2ce053eb
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-06 10:58:15 -0500

    testing

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,15 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+
+    if !BRANDS.include?(brand) #! at beginning makes this a truthy or falsy method - basically says makes the include? an opposite statement. Add brands if it DOES NOT include the brand passed in already? 
+    BRANDS << brand
+    end 
+
   end
 
   def cobble
